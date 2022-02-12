7a32479f590d12678f070784262c032e75a5646d
Serg Gor <pyprosg@gmail.com>
2022-02-12 23:03:00 +0200

    After first work

--- ExcelMutifiles.py
+++ ExcelMutifiles.py
@@ -1,11 +1,16 @@
 import openpyxl
-
+import pathlib
+from FolderCreator import Folder
 
 class ExcelCopy:
+    """class that creates a copy of existing format file wile keeping all hidden sheets.
+    Can create more than one sheet in file with a same format"""
 
     def __init__(self, source_file, **kwargs):
         self.source_file = source_file
         self.source_worksheet_name = kwargs.get("worksheet", None)
+        self.target_folder = kwargs.get("folder", "./")
+        self.target_file_name = kwargs.get("name", f"{self.target_folder}/{self.target_folder}.xlsx")
 
     def __enter__(self):
         self.source_wb = openpyxl.load_workbook(self.source_file)
@@ -15,7 +20,7 @@ class ExcelCopy:
             self.source_ws = self.source_wb.active
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        self.source_wb.save(self.target_folder)
 
     def create_sheet_copy(self, greed_lines=False, **kwargs):
         """creates a copy and delete the sample sheet"""
@@ -27,3 +32,13 @@ class ExcelCopy:
     def create_workbook(self, workbook_name, *args):
         """create file according to format, for multiple sheet provide sheet names as list to *args"""
 
+    def check_type_source_file(self):
+        """checks that source file is excel"""
+
+    # @Folder.create_folder
+    def check_for_folder(self):
+        """checks if folder does exist. use a decorator to other class if not and create"""
+
+file = r"C:\Users\marin\PycharmProjects\Naftya OOP\Format.xlsx"
+with ExcelCopy(file) as ec:
+    ec.create_sheet_copy()
--- FolderCreator.py
+++ FolderCreator.py
@@ -1,9 +1,9 @@
 import pathlib
 
 class Folder:
-    def __init__(self, destination_path):
+    def __init__(self, path):
         """checks if folder exist if not creates - no exceptions raised"""
-        self.destination = destination_path
+        self.destination = path
         self.create_folder()
 
     def create_folder(self):
